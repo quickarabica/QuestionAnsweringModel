9d968862780a8f94f05f50b7bf76c7127516d7e0
quickarabica <cse22067@iiitkalyani.ac.in>
2024-10-07 14:32:29 +0530

    Server changed

---
+++ node_server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const neo4j = require('neo4j-driver');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Serve the main HTML page on the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/main.html')); // Serve the main index.html
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000'); // Corrected message
+});
